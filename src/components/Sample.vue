f0843444d44c48faee3f129e14003cc800aaa6f5
reiv003 <reiv003@student.kristiania.no>
2022-03-12 01:46:13 +0100

    Iinital commit

--- src/components/Sample.vue
+++ src/components/Sample.vue
@@ -1,5 +1,5 @@
 <template>
-	<h1>Template site</h1>
+	<h1>Strømpriser</h1>
 	<div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora cupiditate quo culpa nam ipsa sed dolore doloremque non ex tenetur error, inventore quasi quis veniam quia nemo ullam distinctio mollitia.</div>
 </template>
 
