a8d53f07f6c84e2f3099152716beb8d00163d8a6
reiv003 <reiv003@student.kristiania.no>
2022-06-01 16:34:45 +0200

    Set up routing for page not found and home

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
 import Home from './views/Home.vue';
+import NotFound from './components/NotFound.vue';
+
+/*https://router.vuejs.org/guide/essentials/dynamic-matching.html#catch-all-404-not-found-route
+Used this syntax to catch any routes that do not match the routes defined here, and show the Not Found component in that case. */
 
 export default [
-	{ name: 'home', path: '/', component: Home }
+	{ name: 'home', path: '/', component: Home },
+	{ name: 'notFound', path: '/:pathMatch(.*)*', component: NotFound }
 ]
